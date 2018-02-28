ea869d8a0f5b5ad3bca3104481ef648755254d6f
boubinjg <boubinjg@gmail.com>
2018-02-27 20:02:43 -0500

    first part working, part 2 started

--- PA2.py
+++ PA2.py
@@ -8,6 +8,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import math
 
+Tnew = np.matrix([[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]])
+
 class Arrow3D(FancyArrowPatch):
     def __init__(self, xs, ys, zs, *args, **kwargs):
         FancyArrowPatch.__init__(self, (0,0), (0,0), *args, **kwargs)
@@ -18,15 +20,6 @@ class Arrow3D(FancyArrowPatch):
         xs, ys, zs = proj3d.proj_transform(xs3d, ys3d, zs3d, renderer.M)
         self.set_positions((xs[0],ys[0]),(xs[1],ys[1]))
         FancyArrowPatch.draw(self, renderer)
-
-fig = plt.figure()
-ax = fig.gca(projection='3d')
-ax.view_init(azim=30)
-ax.set_xlim3d(0,5)
-ax.set_ylim3d(0,5)
-ax.set_zlim3d(0,5)
-ax.set_aspect("equal")
-
 
 def init():
     i=0
@@ -43,10 +36,11 @@ def init():
     return xb, yb, zb
 
 def animate(i):
-    fbo=np.array([1+i/10, 1+i/10, 1+i/10])
-    fbx=fbo+np.array([1, 0, 0])
-    fby=fbo+np.array([0, 1, 0])
-    fbz=fbo+np.array([0, 0, 1])
+    #fbo=np.array([1+i/10, 1+i/10, 1+i/10])
+    fbo=np.array([Tnew[0,3],Tnew[1,3],Tnew[2,3]])
+    fbx=fbo+np.array([Tnew[0,0], Tnew[0,1], Tnew[0,2]])
+    fby=fbo+np.array([Tnew[1,0], Tnew[1,1], Tnew[1,2]])
+    fbz=fbo+np.array([Tnew[2,0], Tnew[2,1], Tnew[2,2]])
     xb = Arrow3D([fbo[0],fbx[0]],[fbo[1],fbx[1]],[fbo[2],fbx[2]], mutation_scale=15, lw=2, arrowstyle="-|>", color="k")
     yb = Arrow3D([fbo[0],fby[0]],[fbo[1],fby[1]],[fbo[2],fby[2]], mutation_scale=15, lw=2, arrowstyle="-|>", color="b")
     zb = Arrow3D([fbo[0],fbz[0]],[fbo[1],fbz[1]],[fbo[2],fbz[2]], mutation_scale=15, lw=2, arrowstyle="-|>", color="r")
@@ -57,41 +51,84 @@ def animate(i):
 
 I = np.matrix('1,0,0; 0,1,0 ; 0,0,1')
 
-def expConfig(S, T0):
-    #S is the screw axis (w,v),
+def expConfig(S, theta, T0):
+    #S is the screw axis (w,v) (assumes ||w|| == 1)
     #T0 is the initial frame configuration
     #By theorem 1, we can find e^[S]theta:
+    print("Calculating e^[s]theta")
     w = np.matrix(S[0:3])
     v = np.matrix(S[3:6])
     ret = []
     #if w == 0
     if np.count_nonzero(w) == 0:
-        ret = np.concatenate((I, [[0],[0],[0]]), axis=1) #Theta is 0
-        ret = np.concatenate((ret, [[0,0,0,1]]), axis=0)
-        return ret;
+        print("w == 0")
+        vt = v*theta
+        evt = np.concatenate((I, np.transpose(vt)), axis=1)
+        evt = np.concatenate((evt, [[0,0,0,1]]), axis=0)
+        print("e^[V]theta: ")
+        print(evt)
+        Tnew = evt*T0
+        return Tnew
     #if w != 0
     else:
+        print("w != 0")
         #e^[w]theta
-        theta = np.linalg.norm(w)
-        wHat = w/theta
+        #[wHat]
         wSq = np.matrix([[0, -w[0,2], w[0,1]],
                          [w[0,2], 0, -w[0,0]],
                          [-w[0,1], w[0,0], 0]])
-
+        #e^[w]*theta
         ewt = I + wSq*math.sin(theta) + wSq*wSq*(1-math.cos(theta))
-
+        print("e^[w]theta: ")
         print(ewt)
+        #G(Theta)*v
+        gtv = I*theta + (1-math.cos(theta))*wSq + (theta - math.sin(theta))*wSq*wSq;
+        gtv = gtv*np.transpose(v); #v is a row vector
+        print("G(theta)*v")
+        print(gtv)
+        #e^[V]theta
+        evt = np.concatenate((ewt, gtv), axis=1)
+        evt = np.concatenate((evt, [[0,0,0,1]]), axis=0)
+        print("e^[V]theta")
+        print(evt)
+        #multiply e^[V]Theta * T0
+        Tnew = evt * T0;
 
-        print("It isn't empty")
+        return Tnew
+
+S = [0,0,0,1,0,0]
+theta = 1;
+T0 = np.matrix([[1,0,0,5],
+                [0,1,0,4],
+                [0,0,1,3],
+                [0,0,0,1]])
+
+def screwMotion(s,h,q,theta,T0):
+    #convert s,h,q to a format for the previous func
+    w = s*theta
+    v =
 
 
-S = [0,0,1,4,5,6]
-T = []
-expConfig(S,T)
 
+#print(T0)
+print("S: ")
+print(S);
+print("Theta: ")
+print(theta)
+print("T0: ")
+print(T0)
+Tnew = expConfig(S,theta,T0)
+print("Tnew: ")
+print(Tnew)
 
-#ani = animation.FuncAnimation(fig, animate, np.arange(1, 50), interval=25, blit=True, init_func=init)
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+ax.view_init(azim=30)
+ax.set_xlim3d(3,10)
+ax.set_ylim3d(3,10)
+ax.set_zlim3d(3,10)
+ax.set_aspect("equal")
 
-#plt.show()
+ani = animation.FuncAnimation(fig, animate, np.arange(0, 50), interval=50, blit=True, init_func=init)
 
-
+plt.show()
